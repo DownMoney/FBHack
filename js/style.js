bdb99519cf4dd7fa588c5a5761c4460b7daf1bb4
bartekbow <bowtheguitarist@gmail.com>
2014-03-15 20:17:45 +0000

    hehe

--- js/style.js
+++ js/style.js
@@ -1,3 +1,5 @@
+var playlistDroppedDown = true;
+
 $(window).ready(function(){
 	setSizeOfColumns();
 });
@@ -7,10 +9,38 @@ $(window).resize(function(){
 });
 
 
+
+
 function setSizeOfColumns() {
 	if(parseInt($(window).width()) > 992 ){
 		$(".column").css('height', $(".jumbotron").height());
+		$(".container").css('height',"70%");
+		playlistDropdown();
 	} else {
 		$(".column").css('height',"300px");
+		$(".container").css('height',"1000px");
+		playlistGetUp();
 	}
+}
+
+function playlistDropdown0() {
+	if (!playlistDroppedDown) {
+		playlistDropdown();
+
+	} else {
+		playlistGetUp();
+	}
+}
+
+function playlistDropdown() {
+	$(".playlistDropdown").css("visibility","visible");
+	setTimeout(function(){$(".playlistDropdown").animate("height",$(".jumbotron").height());},100);
+	playlistDroppedDown = true;
+}
+
+function playlistGetUp() {
+	$(".playlistDropdown").animate("height","0px");
+
+	setTimeout(function(){$(".playlistDropdown").css("visibility","hidden");},100);
+	playlistDroppedDown = false;
 }
