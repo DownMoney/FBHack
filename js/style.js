1791133eeaed3c2e2a5c07350894cde35a4b725f
koenraat <konrad@stojanowski.eu>
2014-03-16 02:09:03 +0000

    Cos zjebalem

--- js/style.js
+++ js/style.js
@@ -1,7 +1,7 @@
 $("window").ready(function () {
 	var temp = ($(this).innerHeight()-200)/2;
-
 	$('#qrcode').css("margin-top", temp);
+	//$('buyAmazonImg').css("width", $('#m_artistPicture').width());
 });
 
 $(".playlistHeader").click(function (){
