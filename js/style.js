5b6f11dde8630d0d8814d133b237c6bbddd2dd50
bartekbow <bowtheguitarist@gmail.com>
2014-03-16 00:26:29 +0000

    suicide

--- js/style.js
+++ js/style.js
@@ -1,37 +1,18 @@
+$("window").ready(function () {
+	var temp = ($(this).innerHeight()-200)/2;
 
-//$('#volumeDropdown').toggle('blind',1);
-
-$(window).ready(function(){
-	setSizeOfColumns();
-	alert
-	var height = $('.btn-group').width()+$("#progressBar").width()+30;
-	$("#artistPicture").css("height",height + "px"	);
-	$("#artistPicture").css("width",height + "px");
+	$('#qrcode').css("margin-top", temp);
 });
-
-$(window).resize(function(){
-	setSizeOfColumns();
-});
-
-
-function setSizeOfColumns() {
-	if(parseInt($(window).width()) > 992 ){
-		
-		var temp = $(".container").height() - 100;
-		$(".column").css('height', temp + "px");
-		$(".container").css('height',"70%");
-		$(".playlistDropdown").css("max-height",( $(".column").height() - 80 )+ "px");
-	} else {
-		$(".column").css('height',"300px");
-		$(".container").css('height',"1000px");
-		$(".playlistDropdown").css("max-height",( $(".column").height() - 80 )+ "px");
-	}
-}
 
 $(".playlistHeader").click(function (){
 	$('.playlistDropdown').toggle('blind',100);
 });
 
-/*$("#volume").click(function(){
-	$('#volumeDropdown').toggle('blind',100);
-});*/
+
+$("#qrcode").mouseover(function  () {
+	$("#qrcode").animate({left:"12%"},500);
+});
+
+$("#qrcode").mouseleave(function  () {
+	$("#qrcode").animate({left:"-150px"},500);
+});
